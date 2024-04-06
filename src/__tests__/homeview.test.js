54e6c1a28ffe019dadd99d090570b67fb7409d1f
Artem <artemiy.bondar49@gmail.com>
2024-04-06 11:27:31 +0300

    link stub added

--- src/__tests__/homeview.test.js
+++ src/__tests__/homeview.test.js
@@ -1,17 +1,25 @@
-import { mount } from "@vue/test-utils";
+import { mount, RouterLinkStub } from "@vue/test-utils";
 import HomeView from "../views/HomeView.vue";
 import { expect, test, describe } from "vitest";
 
 test("mount component", async () => {
   expect(HomeView).toBeTruthy();
 
-  const wrapper = mount(HomeView);
+  const wrapper = mount(HomeView, {
+    stubs: {
+      RouterLink: RouterLinkStub,
+    },
+  });
   expect(wrapper.exists).toBeTruthy();
 });
 
 test("check text", async () => {
   expect(HomeView).toBeTruthy();
 
-  const wrapper = mount(HomeView);
+  const wrapper = mount(HomeView, {
+    stubs: {
+      RouterLink: RouterLinkStub,
+    },
+  });
   expect(wrapper.text()).toContain("Choose Your Web Map");
 });
--- src/__tests__/listview.test.js
+++ src/__tests__/listview.test.js
@@ -1,4 +1,4 @@
-import { mount } from "@vue/test-utils";
+import { mount, RouterLinkStub } from "@vue/test-utils";
 import ListView from "../views/ListView.vue";
 import { expect, test, describe, beforeEach } from "vitest";
 import { createPinia, setActivePinia } from "pinia";
@@ -11,19 +11,31 @@ describe("listview with store", () => {
   });
 
   test("mount component", async () => {
-    const wrapper = mount(ListView);
+    const wrapper = mount(ListView, {
+      stubs: {
+        RouterLink: RouterLinkStub,
+      },
+    });
     expect(wrapper.exists).toBeTruthy();
   });
 
   test("check text", async () => {
-    const wrapper = mount(ListView);
+    const wrapper = mount(ListView, {
+      stubs: {
+        RouterLink: RouterLinkStub,
+      },
+    });
     expect(wrapper.text()).toContain("Фильтры");
   });
 
   test("check pinia store", async () => {
     expect(ListView).toBeTruthy();
 
-    const wrapper = mount(ListView);
+    const wrapper = mount(ListView, {
+      stubs: {
+        RouterLink: RouterLinkStub,
+      },
+    });
     expect(mapStore.maps.length).toBe(9);
     expect(mapStore.getFilteredMaps.length).toBe(9);
   });
